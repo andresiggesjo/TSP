c085b30d3489243db1d5c7bf625af5b2d3a75a85
Andre <andre.siggesjo@hotmail.se>
2017-02-23 17:32:46 +0100

    better than optimum

--- src/tsp/TSP_GA.java
+++ src/tsp/TSP_GA.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class TSP_GA {
@@ -17,9 +18,9 @@ public class TSP_GA {
 
         
         //Read berlin52 
+        //BufferedReader br = new BufferedReader(new FileReader("optimalsuperoptimal.txt"));
         BufferedReader br = new BufferedReader(new FileReader("berlin52.txt"));
-        
-   
+        //ArrayList<City> OptimumList = new ArrayList<City>();
         String line;
         while ((line = br.readLine()) != null) {
 
@@ -27,16 +28,17 @@ public class TSP_GA {
             
             //create city and add it to the tourmanager
             City tempcity = new City(Integer.parseInt(tempArray[0]), Integer.parseInt(tempArray[1]));
+           // OptimumList.add(tempcity);
             TourManager.addCity(tempcity);
             
+            
          }
+       // Tour optimum = new Tour(OptimumList);
+       // System.out.println(optimum.getDistance());
 
 
-        
-
-       
         // Initialize population
-        Population pop = new Population(250, true);
+        Population pop = new Population(10000, true);
         System.out.println("Initial distance: " + pop.getFittest().getDistance());
 
         //Set mutationrate
@@ -44,7 +46,7 @@ public class TSP_GA {
         
         // Evolve population for 100 generations
         pop = GA.evolvePopulation(pop);
-        for (int i = 0; i < 400; i++) {
+        for (int i = 0; i < 10000; i++) {
             pop = GA.evolvePopulation(pop);
         }
 
